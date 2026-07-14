81e4686c905fd6a31ba4793a6ae01c76dd0b2680
merint0414-dot <merint0414@gmail.com>
2026-07-15 01:12:59 +0530

    Navigation Bar Created

--- src/components/AddVolunteer.jsx
+++ src/components/AddVolunteer.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavigationBar from './NavigationBar'
 
 const AddVolunteer = () => {
   const [input, changeInput] = useState({
@@ -60,7 +61,7 @@ const AddVolunteer = () => {
     return (
 
         <div>
-
+            <NavigationBar/>
 
             <div className="container mt-4">
 
